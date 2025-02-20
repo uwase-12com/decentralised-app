32a3938991a65ee145bd1fa0b76a57669b0bae2b
uwase-12com <uwaseangel949@gmail.com>
2025-02-20 19:13:38 +0200

    Add files via upload

---
+++ Ced.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+   <fieldset>
+      <center>
+   <form action="insert.php"method="POST">
+   <label for="">id:</label>
+   <input type="id"name="id" placeholder="id"><br><br>
+<label for="">username:</label>
+<input type="text"name="username" placeholder="username"><br><br>
+<label for="">phonenumber:</label>
+<input type="text"name="phonenumber" placeholder="phonenumber"><br><br>
+<label for="">password:</label>
+<input type="password"name="password" placeholder="password"><br><br>
+<button type="submit">send</button>
+   </fieldset>
+   </center>
+   </form> 
+</body>
+</html>
